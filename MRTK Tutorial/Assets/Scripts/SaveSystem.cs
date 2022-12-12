5c089945dd69c8e22b8ebdf2d04607d6ac0c0db4
Bruno Berger <brunoberger.dev@gmail.com>
2022-12-12 16:38:02 +0100

    working example save system

--- MRTK Tutorial/Assets/Scripts/SaveSystem.cs
+++ MRTK Tutorial/Assets/Scripts/SaveSystem.cs
@@ -1,52 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 [System.Serializable]
 public struct Save
 {
-    GameObject[] meshCopyCollection;
-    List<GameObject> placedObjects;
+    //GameObject[] meshCopyCollection;
+    //List<GameObject> placedObjects;
 
-
+    public string testString;
 
 }
 
 public class SaveSystem : MonoBehaviour
 {
+    static readonly string folderpath = "SaveFiles";
+    readonly string filepath = folderpath + "/mySaveFile.save";
+
     public void SaveToDisk()
     {
+        Directory.CreateDirectory("SaveFiles");
         Save save = new Save();
-        using ( var stream = File.Open("mySaveFile", FileMode.Create))
+        save.testString = "Hello disk!";
+
+        using var stream = File.Open(filepath, FileMode.Create);
+        BinaryFormatter formatter = new BinaryFormatter();
+        try
         {
-            using (var writer = new BinaryWriter(stream, System.Text.Encoding.UTF8, false))
-            {
-                writer.Write("test");
-                writer.Write(save);
-            }
+            formatter.Serialize(stream, save);
         }
-        
+        catch (SerializationException e)
+        {
+            Debug.LogError("Could not save Save" + e);
+            throw;
+        }
+
     }
     public void LoadFromDisk()
     {
-        //Save save = (Save)formatter.Deserialize(stream);
-    }
-    public void RenewSaveData(GameObject[] meshCopyCollection, List<GameObject> placedObjects)
-    {
-        //this.meshCopyCollection = meshCopyCollection;
-        //this.placedObjects = placedObjects;
+        Save loadedSave;
+
+        using var stream = File.Open(filepath, FileMode.Open);
+        try
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            loadedSave = (Save)formatter.Deserialize(stream);
+        }
+        catch (SerializationException e)
+        {
+            Debug.LogError("Could not load save" + e);
+            throw;
+        }
+
+        Debug.Log("LOADED MSG:" + loadedSave.testString);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SaveToDisk();
+        LoadFromDisk();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
