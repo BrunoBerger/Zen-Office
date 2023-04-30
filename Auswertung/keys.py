1ded1e93e824929fb4a28a8c015fead1a05bf687
Bruno Berger <bruno.berger@stud.hshl.de>
2023-05-01 01:20:36 +0200

    tested UEQ plot with two x axis

---
+++ Auswertung/keys.py
@@ -0,0 +1,67 @@
+StressState_Names = [
+    "Unzufrieden",
+    "Aufmerksam",
+    "Deprimiert",
+    "Traurig",
+    "Aktiv",
+    "Ungeduldig",
+    "Genervt",
+    "Wütend",
+    "Irritiert",
+    "Mürrisch",
+]
+StressState_AR_Keys = [
+    "SS01_01",
+    "SS01_02",
+    "SS01_03",
+    "SS01_04",
+    "SS01_05",
+    "SS01_06",
+    "SS01_07",
+    "SS01_08",
+    "SS01_09",
+    "SS01_10",
+]
+StressState_NonAR_Keys = [
+    "SS03_01",
+    "SS03_02",
+    "SS03_03",
+    "SS03_04",
+    "SS03_05",
+    "SS03_06",
+    "SS03_07",
+    "SS03_08",
+    "SS03_09",
+    "SS03_10",
+]
+
+ShortUserExpQ_Names_left = [
+    "behindernd",
+    "kompliziert",
+    "ineffizient",
+    "verwirrend",
+    "langweilig",
+    "uninteressant",
+    "konventionell", 
+    "herkömmlich",
+]
+ShortUserExpQ_Names_right = [
+    "unterstützend",
+    "einfach",
+    "effizient",
+    "übersichtlich",
+    "spannend",
+    "interessant",
+    "originell",
+    "neuartig",
+]
+ShortUserExpQ_Keys = [
+    "UE01_01",
+    "UE01_02",
+    "UE01_03",
+    "UE01_04",
+    "UE01_05",
+    "UE01_06",
+    "UE01_07",
+    "UE01_08",
+]
