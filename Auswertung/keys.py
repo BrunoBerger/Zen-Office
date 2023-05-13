676c79a223ce39c9905654e64a937cd2b70edeeb
BrunoBerger <brunoberger.dev@gmail.com>
2023-05-13 20:54:12 +0200

    updated survey and adjusted keys

--- Auswertung/keys.py
+++ Auswertung/keys.py
@@ -7,6 +7,7 @@ Demographic_Keys = [
     "DD05",
     "DD06",
     "DD07",
+    "DD08_01",
 ]
 Demographic_Names = [
     "Alter",
@@ -17,9 +18,10 @@ Demographic_Names = [
     "Cybersickness ",
     "NaturZurEntspannung",
     "NutzungRelaxApps",
+    "UserID",
 ]
 
-StressState_Names = [
+SSSQ_Scale_Names = [
     "Unzufrieden",
     "Aufmerksam",
     "Deprimiert",
@@ -31,7 +33,7 @@ StressState_Names = [
     "Irritiert",
     "Mürrisch",
 ]
-StressState_AR_Keys = [
+SSSQ_AR_Listen_Scale_Keys = [
     "SS01_01",
     "SS01_02",
     "SS01_03",
@@ -43,7 +45,7 @@ StressState_AR_Keys = [
     "SS01_09",
     "SS01_10",
 ]
-StressState_NonAR_Keys = [
+SSSQ_NonAR_Listen_Scale_Keys = [
     "SS03_01",
     "SS03_02",
     "SS03_03",
@@ -55,6 +57,67 @@ StressState_NonAR_Keys = [
     "SS03_09",
     "SS03_10",
 ]
+SSSQ_AR_Sudoku_Scale_Keys = [
+    "SS06_01",
+    "SS06_02",
+    "SS06_03",
+    "SS06_04",
+    "SS06_05",
+    "SS06_06",
+    "SS06_07",
+    "SS06_08",
+    "SS06_09",
+    "SS06_10",
+]
+SSSQ_NonAR_Sudoku_Scale_Keys = [
+    "SS08_01",
+    "SS08_02",
+    "SS08_03",
+    "SS08_04",
+    "SS08_05",
+    "SS08_06",
+    "SS08_07",
+    "SS08_08",
+    "SS08_09",
+    "SS08_10",
+]
+
+SSSQ_Statement_Names = [
+    "Ich war entschlossen meine Leistungsziele zu erreichen",
+    "Ich wollte bei der Aufgabe erfolgreich sein",
+    "Ich war motiviert die Aufgabe umzusetzen",
+    "Ich führte die Aufgabe kompetent aus",
+    "Grundsätzlich fühlte ich, dass ich die Dinge unter Kontrolle hatte",
+]
+SSSQ_AR_Listen_Statement_Keys = [
+    "SS02_01",
+    "SS02_02",
+    "SS02_03",
+    "SS02_11",
+    "SS02_12",
+]
+SSSQ_AR_Sudoku_Statement_Keys = [
+    "SS07_01",
+    "SS07_02",
+    "SS07_03",
+    "SS07_11",
+    "SS07_12",
+]
+SSSQ_AR_Listen_Statement_Keys = [
+    "SS04_01",
+    "SS04_02",
+    "SS04_03",
+    "SS04_11",
+    "SS04_12",
+]
+SSSQ_AR_Sudoku_Statement_Keys = [
+    "SS09_01",
+    "SS09_02",
+    "SS09_03",
+    "SS09_11",
+    "SS09_12",
+]
+
 
 ShortUserExpQ_Names_left = [
     "behindernd",
